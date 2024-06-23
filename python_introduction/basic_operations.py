effad2a202554f23a564b6a78c109a6542d5f83c
Salsabilbr <sabilbr90@gmail.com>
2024-06-23 09:53:14 -0700

    basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,12 +1,16 @@
+["number1 = 10", "number2 = 5"]
 number1=10
 number2=5
 sum=int(number1)+int(number2)
 print(sum)
+15
 number1=10
 number2=5
 sum=int(number1)-int(number2)
 print(sum)
+5
 number1=10
 number2=5
 sum=int(number1)*int(number2)
 print(sum)
+50
