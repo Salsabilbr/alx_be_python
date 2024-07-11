7971aa3d829adfbfdc71d9b595116b1a2af676c1
Salsabilbr <sabilbr90@gmail.com>
2024-07-11 15:45:29 -0700

    shopping_list_manager.py

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,22 @@
+shopping_list = []
+print("1. Add Item")
+print("2. Remove Item")
+print("3. View List")
+print("4. Exit")
+def add_item():
+    item = input("Enter the item to add: ")
+    shopping_list.append(item)
+    print(item, "added to the shopping list")
+def remove_item():
+    item = input("Enter the item to remove: ")
+    if item in shopping_list:
+        shopping_list.remove(item)
+        print(item, "removed from the shopping list")
+    else:
+        print(item, "not found in the shopping list")
+        def view_list():
+    print("Current Shopping List:")
+    for item in shopping_list:
+        print("-", item)
+        if __name__ == "__main__":
+    main()
