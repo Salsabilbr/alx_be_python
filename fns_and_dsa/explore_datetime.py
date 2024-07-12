c3698eadb001b5d29db4514510c9c22d23b9e939
Salsabilbr <sabilbr90@gmail.com>
2024-07-12 02:27:21 -0700

    explore_datetime.py

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,11 @@
+def display_current_datetime():
+    current_date = datetime.datetime.now()
+    formatted_date = current_date("%Y-%m-%d %H:%M:%S")
+    print("Current Date and Time:", formatted_date)
+    display_current_datetime()
+def calculate_future_date():
+    current_date = datetime.date.today()
+    num_days = int(input("Enter the number of days: "))
+    future_date = current_date + datetime.timedelta(days=num_days)
+    print("Future Date:", future_date.strftime("%Y-%m-%d"))
+    calculate_future_date()
