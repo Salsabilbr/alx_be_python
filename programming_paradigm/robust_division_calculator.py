0208e1bf1630a2a59732c44f35dadb2fdc2e6576
Salsabilbr <sabilbr90@gmail.com>
2024-07-19 11:00:25 -0700

    robust_division_calculator.py

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,26 +1,22 @@
-
-def safe_divide(numerator, denominator):  
-   try:  
-      numerator = float(numerator)  
-      denominator = float(denominator)  
-      if denominator == 0:  
-        return "Error: Cannot divide by zero."  
-      else:  
-        return f"The result of the division is {numerator / denominator}."  
-   except ValueError:  
-      return "Error: Please enter numeric values only."  
-
-import sys  
-from robust_division_calculator import safe_divide  
-
-def main():  
-   if len(sys.argv)!= 3:  
-      print("Usage: python main.py  ")  
-      sys.exit(1)  
-   numerator = sys.argv  
-   denominator = sys.argv  
-   result = safe_divide(numerator, denominator)  
-   print(result)  
-  
-if __name__ == "__main__":  
-   main()  
+def safe_divide(numerator, denominator):
+   try:
+      numerator = float(numerator)
+      denominator = float(denominator)
+      if denominator == 0:
+        return "Error: Cannot divide by zero."
+      else:
+        return f"The result of the division is {numerator / denominator}."
+   except ValueError:
+      return "Error: Please enter numeric values only."
+import sys
+from robust_division_calculator import safe_divide
+def main():
+   if len(sys.argv)!= 3:
+      print("Usage: python main.py ")
+      sys.exit(1)
+   numerator = sys.argv
+   denominator = sys.argv
+   result = safe_divide(numerator, denominator)
+   print(result)
+if __name__ == "__main__":
+   main()
