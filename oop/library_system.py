b412232bfcfb0821084d173a04602d1dc4bb3b98
Salsabilbr <sabilbr90@gmail.com>
2024-07-21 12:13:01 -0700

    library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,8 +1,8 @@
-class Book:  
-   def __init__(self, title, author):  
-      self.title = title  
-      self.author = author  
-  
+class book:
+   def __init__(self, title, author):
+      self.title = title
+      self.author = author
+
    def __str__(self):  
       return f"Book: {self.title} by {self.author}"  
   
