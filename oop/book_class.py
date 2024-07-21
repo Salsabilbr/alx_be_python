127dd2bd3898e68f6a7443e13b5ca5334e1365fa
Salsabilbr <sabilbr90@gmail.com>
2024-07-21 12:56:03 -0700

    book_class.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,5 +1,5 @@
 class Book:
-def __init__(self, Booktitle: str,Bookauthor: str, Bookyear: int):
+   def __init__(self, title: str,author: str, year: int):
       self.title = title
       self.author = author
       self.year = year
